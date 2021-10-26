0afb0b89f9fb0298a2e41194e50986adbea64d44
krissemicolon <krissemicolon@gmail.com>
2021-10-26 17:30:57 +0200

    sync

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -33,7 +33,7 @@ return require('packer').startup(function(use)
     use { 'kyazdani42/nvim-tree.lua', requires = 'kyazdani42/nvim-web-devicons', config = function() require'nvim-tree'.setup {} end }
 
     -- tabs
-    --use 'romgrk/barbar.nvim'
+    -- alternatives: ['romgrk/barbar.nvim']
     use 'akinsho/bufferline.nvim'
 
     -- treesitter
