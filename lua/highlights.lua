7830d2d48b235e712c1bb1711448925a00762ef1
kris huber [LU15561] <kris.huber@lukb.ch>
2021-12-14 14:31:44 +0100

    feat: lsp-installer now automatically sets ls up

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -1,5 +1,5 @@
 function text_selection()
-    if vim.opt.background:get() == 'light' then
+    if vim.opt.background:get() == "light" then
         vim.cmd("highlight Visual gui=None guifg=None guibg=LightBlue3")
     else
         vim.cmd("highlight Visual gui=None guifg=None guibg=LightBlue4")
--- lua/plugin-configs/lspconfig.lua
+++ lua/plugin-configs/lspconfig.lua
@@ -1,34 +1,3 @@
 local s = require'lspconfig'
 
--- lua
--- s.sumneko_lua.setup {
---   cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
---   settings = {
---     Lua = {
---       runtime = {
---         -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
---         version = 'LuaJIT',
---         -- Setup your lua path
---         path = runtime_path,
---       },
---       diagnostics = {
---         -- Get the language server to recognize the `vim` global
---         globals = {'vim'},
---       },
---       workspace = {
---         -- Make the server aware of Neovim runtime files
---         library = vim.api.nvim_get_runtime_file("", true),
---       },
---       -- Do not send telemetry data containing a randomized but unique identifier
---       telemetry = {
---         enable = false,
---       },
---     },
---   },
--- }
-
--- python
-s.pyright.setup{}
-
--- rust
-s.rust_analyzer.setup{}
+-- all language servers get setup by lsp-installers config (ls-installer.lua)
