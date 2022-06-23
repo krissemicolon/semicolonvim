d6f8c4437a4e72d55dda756a5774d3bfadd3f2f5
Kris Huber <kris@krishuber.xyz>
2022-06-23 19:42:28 +0200

    Added detection filter

--- lua/plugin-configs/project.lua
+++ lua/plugin-configs/project.lua
@@ -2,7 +2,7 @@ require'project_nvim'.setup{
   manual_mode = false,
   silent_chdir = false,
   detection_modes = { "lsp", "pattern" },
-  patterns = { ">Projects" },   -- get all projects from ~/Projects and more
+  patterns = { ">Projects", ">Programming" },   -- get all projects from ~/Projects and more
   exclude_dirs = {},
   datapath = vim.fn.stdpath("data"),
 }
