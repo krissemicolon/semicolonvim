2b5f377dc27adf19d3ecfa030ecc7365ab05301f
Kris Huber <kris@krishuber.xyz>
2022-07-29 19:11:21 +0200

    Made indexing script better

--- lua/gui.lua
+++ lua/gui.lua
@@ -8,6 +8,8 @@ if exists("g:neovide")
     let g:neovide_refresh_rate = 140
     let g:neovide_floating_blur_amount_x = 2.0
     let g:neovide_floating_blur_amount_y = 2.0
+    " cursor move effect
+    " let g:neovide_cursor_vfx_mode = "railgun"
 
     " for pasting in neovide (middle click)
     :set guioptions+=a
--- scripts/index_projects.sh
+++ scripts/index_projects.sh
@@ -3,9 +3,7 @@
 # This is a hacky solution for indexing all projects
 # so you can open them with ctrl + o
 
-# Tip: hold down shift+z to run through them fast
-
 for p in $1/*
 do
-    nvim $p
+    nvim +q $p
 done
