d9c9a18bb09a15f57cbe65df8fe719ac054d840f
Kris Huber <kris@krishuber.xyz>
2022-08-10 21:18:30 +0200

    some neovide settings

--- lua/gui.lua
+++ lua/gui.lua
@@ -4,10 +4,17 @@
 
 vim.cmd([[
 if exists("g:neovide")
+    let $NEOVIDE_MULTIGRID = "true"
+
     " neovide settings
     let g:neovide_refresh_rate = 140
+    let g:neovide_refresh_rate_idle = 5
+
+    let g:neovide_remember_window_size = v:true
+
     let g:neovide_floating_blur_amount_x = 2.0
     let g:neovide_floating_blur_amount_y = 2.0
+    let g:neovide_cursor_animation_length = 0.06
     " cursor move effect
     " let g:neovide_cursor_vfx_mode = "railgun"
 
